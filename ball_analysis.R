a78df57a809e7d0bfd87b534484032caa7b0bf58
Kelly Street <street.kelly@gmail.com>
2025-09-23 14:54:05 -0700

    meeting

--- ball_analysis.R
+++ ball_analysis.R
@@ -9,12 +9,21 @@ d1 + d2
 
 # dots
 
-markers <- unique(c('CTNNB1','CD10','CD34','CD38','CD19','CD133','CD19','CD34','CD20','CD24','TCF7','NOTCH1','LEF1','RUNX1','PROM1','CKIT','LGR5','HOX','LMO2','TCF','TCR','BCL11B','GATA3','E2A','EBF1','RAG','PAX5','EZH2','DOT1L','MLL','SUV420H1','SUV420H2','DNMT3A','TET2','BCL6','IL7R','FOXP3','PTCRA',
+ballmarkers <- unique(c('CTNNB1','CD10','CD34','CD38','CD19','CD133','CD19','CD34','CD20','CD24','TCF7','NOTCH1','LEF1','RUNX1','PROM1','CKIT','LGR5','HOX','LMO2','TCF','TCR','BCL11B','GATA3','E2A','EBF1','RAG','PAX5','EZH2','DOT1L','MLL','SUV420H1','SUV420H2','DNMT3A','TET2','BCL6','IL7R','FOXP3','PTCRA',
                     'TBL1X','TBLR1','MYC','BIRC5','CCND1','AXIN2','TCF','CBP','P300','DKK1','BCL2','P53','SIAH1','CDK','JUN','CDH1'))
+tallmarkers <- unique(c('CTNNB1','CD7','CD1A','CD34','CD38','CD4','CD44','CD8','CD3','TCF7','NOTCH1','LEF1','RUNX1','PROM1','CKIT','LGR5','HOX','LMO2','TCF','TCR','BCL11B','GATA3','E2A','EBF1','RAG','PAX5','EZH2','DOT1L','MLL','SUV420H1','SUV420H2','DNMT3A','TET2','BCL6','IL7R','FOXP3','PTCRA','FZD','LRP5','LRP6','DVL','AXIN','APC','GSK3B','CK1','TBL1X','TBLR1','MYC','BIRC5','CCND1','AXIN2','TCF','LEF1','CBP','P300','EP300','DKK1','BCL2','P53','SIAH1','CDK','JUN','CDH1'))
+
+othermarkers <- unique(c('NFKB','P65','RELA','SMRT','NCOR','HDAC3','GPS2','SCF','CUL1','SKP1','RING','RBX1','ROC1','PI3K','AKT','PTEN','MTOR','MTORC1','MAPK','ERK','JNK','P38','ERK5','TNF','IL','IFN','NOTCH','JAKSTAT','VEGF','PARP1','PPP1R15A','RAD51','BRCA1','BRCA2','BAX','BIM','PUMA','MCL1','ABC','ABCB1','MDR1','ABCC1','ABCG2'))
+
+allmarkers <- unique(c(ballmarkers,tallmarkers,othermarkers)) 
 
 
+DotPlot(ball, allmarkers, cluster.idents = TRUE) + RotatedAxis()
 
-DotPlot(ball, markers) + RotatedAxis()
+
+interestingGenes <- c('CTNNB1','CD38','CD24','EZH2','TBL1X','BIRC5','BCL2','JUN','CD44','GSK3B','GPS2','PTEN','PARP1','PPP1R15A','BRCA1','BRCA2','BAX')
+
+DotPlot(ball, interestingGenes, cluster.idents = TRUE) + RotatedAxis()
 
 
 
@@ -49,18 +58,95 @@ VlnPlot(ball, rownames(markers)[1:12], group.by = 'myclus', slot = 'data', log=F
 ######################
 
 de1 <- FindMarkers(ball, ident.1 = 'YMK5', ident.2 = 'YMK4', group.by = 'orig.ident')
-# volcano
-plot(de1$avg_log2FC, -log10(de1$p_val_adj), col = rgb(0,0,0,.5))
-
 de2 <- FindMarkers(ball, ident.1 = 'YMK6', ident.2 = 'YMK4', group.by = 'orig.ident')
-# volcano
-plot(de2$avg_log2FC, -log10(de2$p_val_adj), col = rgb(0,0,0,.5))
+
+# save tables
+write.csv(de1, file='~/Desktop/ball_allCells_6h-ctrl.csv', quote = FALSE)
+write.csv(de2, file='~/Desktop/ball_allCells_24h-ctrl.csv', quote = FALSE)
+
+# volcano plots with genes labelled
+png(filename = '~/Desktop/B-ALL/ball_allCells_6h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de1$p_val_adj < .99)
+plot(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'B-ALL All Cells: 6h - CTRL')
+text(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), labels = rownames(de1)[filt])
+dev.off()
+
+png(filename = '~/Desktop/B-ALL/ball_allCells_24h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de2$p_val_adj < .99)
+plot(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'B-ALL All Cells: 24h - CTRL')
+text(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), labels = rownames(de2)[filt])
+dev.off()
 
 
-write.csv(de1, file='~/Desktop/ball_6h-ctrl.csv', quote = FALSE)
-write.csv(de2, file='~/Desktop/ball_24h-ctrl.csv', quote = FALSE)
 
-# add more markers to Dot Plot
 
 # DE between samples within groups (4&8, 1&7)
+
+
+ball$group <- rep('other', ncol(ball))
+ball$group[ball$seurat_clusters %in% c(4,8)] <- '4_8'
+ball$group[ball$seurat_clusters %in% c(1,7)] <- '1_7'
+Idents(ball) <- 'group'
+
+# DE: JUST CLUSTERS 4&8 #
+#########################
+
+de1 <- FindMarkers(ball, ident.1 = 'YMK5', ident.2 = 'YMK4', group.by = 'orig.ident', subset.ident = '4_8')
+de2 <- FindMarkers(ball, ident.1 = 'YMK6', ident.2 = 'YMK4', group.by = 'orig.ident', subset.ident = '4_8')
+
+# save tables
+write.csv(de1, file='~/Desktop/ball_clus4and8_6h-ctrl.csv', quote = FALSE)
+write.csv(de2, file='~/Desktop/ball_clus4and8_24h-ctrl.csv', quote = FALSE)
+
+# volcano plots with genes labelled
+png(filename = '~/Desktop/B-ALL/ball_clus4and8_6h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de1$p_val_adj < .99)
+plot(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'B-ALL Clusters 4 & 8: 6h - CTRL')
+text(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), labels = rownames(de1)[filt])
+dev.off()
+
+png(filename = '~/Desktop/B-ALL/ball_clus4and8_24h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de2$p_val_adj < .99)
+plot(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'B-ALL Clusters 4 & 8: 24h - CTRL')
+text(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), labels = rownames(de2)[filt])
+dev.off()
+
+
+# DE: JUST CLUSTERS 1&7 #
+#########################
+
+de1 <- FindMarkers(ball, ident.1 = 'YMK5', ident.2 = 'YMK4', group.by = 'orig.ident', subset.ident = '1_7')
+de2 <- FindMarkers(ball, ident.1 = 'YMK6', ident.2 = 'YMK4', group.by = 'orig.ident', subset.ident = '1_7')
+
+# save tables
+write.csv(de1, file='~/Desktop/ball_clus1and7_6h-ctrl.csv', quote = FALSE)
+write.csv(de2, file='~/Desktop/ball_clus1and7_24h-ctrl.csv', quote = FALSE)
+
+# volcano plots with genes labelled
+png(filename = '~/Desktop/B-ALL/ball_clus1and7_6h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de1$p_val_adj < .99)
+plot(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'B-ALL Clusters 1 & 7: 6h - CTRL')
+text(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), labels = rownames(de1)[filt])
+dev.off()
+
+png(filename = '~/Desktop/B-ALL/ball_clus1and7_24h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de2$p_val_adj < .99)
+plot(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'B-ALL Clusters 1 & 7: 24h - CTRL')
+text(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), labels = rownames(de2)[filt])
+dev.off()
+
+
+
 
--- tall_analysis.R
+++ tall_analysis.R
@@ -9,10 +9,21 @@ d1 + d2
 
 # dots (use actual known genes)
 
-markers <- unique(c('CTNNB1','CD7','CD1A','CD34','CD38','CD4','CD44','CD8','CD3','TCF7','NOTCH1','LEF1','RUNX1','PROM1','CKIT','LGR5','HOX','LMO2','TCF','TCR','BCL11B','GATA3','E2A','EBF1','RAG','PAX5','EZH2','DOT1L','MLL','SUV420H1','SUV420H2','DNMT3A','TET2','BCL6','IL7R','FOXP3','PTCRA'))
+ballmarkers <- unique(c('CTNNB1','CD10','CD34','CD38','CD19','CD133','CD19','CD34','CD20','CD24','TCF7','NOTCH1','LEF1','RUNX1','PROM1','CKIT','LGR5','HOX','LMO2','TCF','TCR','BCL11B','GATA3','E2A','EBF1','RAG','PAX5','EZH2','DOT1L','MLL','SUV420H1','SUV420H2','DNMT3A','TET2','BCL6','IL7R','FOXP3','PTCRA',
+                        'TBL1X','TBLR1','MYC','BIRC5','CCND1','AXIN2','TCF','CBP','P300','DKK1','BCL2','P53','SIAH1','CDK','JUN','CDH1'))
+tallmarkers <- unique(c('CTNNB1','CD7','CD1A','CD34','CD38','CD4','CD44','CD8','CD3','TCF7','NOTCH1','LEF1','RUNX1','PROM1','CKIT','LGR5','HOX','LMO2','TCF','TCR','BCL11B','GATA3','E2A','EBF1','RAG','PAX5','EZH2','DOT1L','MLL','SUV420H1','SUV420H2','DNMT3A','TET2','BCL6','IL7R','FOXP3','PTCRA','FZD','LRP5','LRP6','DVL','AXIN','APC','GSK3B','CK1','TBL1X','TBLR1','MYC','BIRC5','CCND1','AXIN2','TCF','LEF1','CBP','P300','EP300','DKK1','BCL2','P53','SIAH1','CDK','JUN','CDH1'))
 
-DotPlot(tall, markers) + RotatedAxis()
+othermarkers <- unique(c('NFKB','P65','RELA','SMRT','NCOR','HDAC3','GPS2','SCF','CUL1','SKP1','RING','RBX1','ROC1','PI3K','AKT','PTEN','MTOR','MTORC1','MAPK','ERK','JNK','P38','ERK5','TNF','IL','IFN','NOTCH','JAKSTAT','VEGF','PARP1','PPP1R15A','RAD51','BRCA1','BRCA2','BAX','BIM','PUMA','MCL1','ABC','ABCB1','MDR1','ABCC1','ABCG2'))
 
+allmarkers <- unique(c(ballmarkers,tallmarkers,othermarkers)) 
+
+
+DotPlot(tall, allmarkers, cluster.idents = TRUE) + RotatedAxis()
+
+
+interestingGenes <- c('CTNNB1','CD7','CD38','CD44','TCF7','LEF1','RUNX1','BCL11B','BIRC5','LMO2','EZH2','TET2','BCL6','TBL1X','JUN','GSK3B','SKP1','RBX1','PTEN','PARP1','PPP1R15A','BRCA1','ABCC1')
+
+DotPlot(tall, interestingGenes, cluster.idents = TRUE) + RotatedAxis()
 
 
 # clusters 11 and maybe 14 are interesting
@@ -44,19 +55,77 @@ VlnPlot(tall, markers[1:10], slot = 'data', log=FALSE, alpha=0.1)
 ######################
 
 de1 <- FindMarkers(tall, ident.1 = 'YMK2', ident.2 = 'YMK1', group.by = 'orig.ident')
-# volcano
-plot(de1$avg_log2FC, -log10(de1$p_val_adj), col = rgb(0,0,0,.5))
-
 de2 <- FindMarkers(tall, ident.1 = 'YMK3', ident.2 = 'YMK1', group.by = 'orig.ident')
-# volcano
-plot(de2$avg_log2FC, -log10(de2$p_val_adj), col = rgb(0,0,0,.5))
 
+# save tables
+write.csv(de1, file='~/Desktop/tall_allCells_6h-ctrl.csv', quote = FALSE)
+write.csv(de2, file='~/Desktop/tall_allCells_24h-ctrl.csv', quote = FALSE)
 
-write.csv(de1, file='~/Desktop/tall_6h-ctrl.csv', quote = FALSE)
-write.csv(de2, file='~/Desktop/tall_24h-ctrl.csv', quote = FALSE)
+# volcano plots with genes labelled
+png(filename = '~/Desktop/T-ALL/tall_allCells_6h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de1$p_val_adj < .99)
+plot(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'T-ALL All Cells: 6h - CTRL')
+text(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), labels = rownames(de1)[filt])
+dev.off()
 
+png(filename = '~/Desktop/T-ALL/tall_allCells_24h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de2$p_val_adj < .99)
+plot(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'T-ALL All Cells: 24h - CTRL')
+text(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), labels = rownames(de2)[filt])
+dev.off()
 
 
 # DE between samples within cluster 11 (t-cell leukemia stem cells)
+# DE: JUST CLUSTER 11 #
+#######################
+
+de1 <- FindMarkers(tall, ident.1 = 'YMK2', ident.2 = 'YMK1', group.by = 'orig.ident', subset.ident = '11')
+de2 <- FindMarkers(tall, ident.1 = 'YMK3', ident.2 = 'YMK1', group.by = 'orig.ident', subset.ident = '11')
+
+# save tables
+write.csv(de1, file='~/Desktop/tall_clus11_6h-ctrl.csv', quote = FALSE)
+write.csv(de2, file='~/Desktop/tall_cluss11_24h-ctrl.csv', quote = FALSE)
+
+# volcano plots with genes labelled
+png(filename = '~/Desktop/T-ALL/tall_clus11_6h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de1$p_val_adj < .99)
+plot(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'T-ALL Cluster 11: 6h - CTRL')
+text(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), labels = rownames(de1)[filt])
+dev.off()
+
+png(filename = '~/Desktop/T-ALL/tall_clus11_24h-ctrl_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de2$p_val_adj < .99)
+plot(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'T-ALL Cluster 11: 24h - CTRL')
+text(de2$avg_log2FC[filt], -log10(de2$p_val_adj[filt]), labels = rownames(de2)[filt])
+dev.off()
+
+
+# DE cluster 11 markers
+# DE: CLUSTER 11 VS. OTHERS #
+#############################
+
+de1 <- FindMarkers(tall, ident.1 = '11')
+
+# save tables
+write.csv(de1, file='~/Desktop/T-ALL/tall_clus11-other.csv', quote = FALSE)
+
+# volcano plots with genes labelled
+png(filename = '~/Desktop/T-ALL/tall_clus11-other_volcano.png', width = 12, height = 8, units = "in", res = 200)
+filt <- which(de1$p_val_adj < .99)
+plot(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), col='white',
+     xlab = 'log2 Fold Change', ylab = '-log10 p-value',
+     main = 'T-ALL All Cells: Cluster 11 - Other')
+text(de1$avg_log2FC[filt], -log10(de1$p_val_adj[filt]), labels = rownames(de1)[filt])
+dev.off()
+
+
 
 
